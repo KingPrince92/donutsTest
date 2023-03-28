22ca1641f285613fae764c1b290b41cef2913143
Milo Prince <mprince@onemagnify.com>
2023-03-28 16:54:08 -0400

    added style plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import * as React from "react"
+import Layout from "../Components/Layout"
+import styled from 'styled-components'
+
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+		<Layout>
+		<div data-style='ThreeFont'>
+      <h1>Delightful Donuts</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque harum, eligendi nulla reiciendis placeat tempora, impedit consequatur soluta nesciunt assumenda nemo, hic totam? Eligendi temporibus, harum modi ipsum similique dolorem!</p>
+    </div>
+
+    <div>
+      <h1>Delightful Donuts</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur ullam ex voluptates consectetur quae animi alias laudantium non a nostrum cupiditate iure impedit voluptatem ad doloremque, sed perspiciatis? Ut.</p>
+    </div>
+
+	
+		</Layout>
+  );
 }
---
+++ src/pages/testing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testing = () => {
+  return (
+	<div>
+	  Test
+	</div>
+  )
+}
+
+export default Testing
